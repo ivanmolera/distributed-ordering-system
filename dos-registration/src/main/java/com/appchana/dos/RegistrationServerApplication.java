15b16f128ac55b724a0632ccea02b792c7176ecd
Ivan Molera <molera@strands.com>
2018-04-12 12:45:06 +0200

    Microservices discovery server

---
+++ dos-registration/src/main/java/com/appchana/dos/RegistrationServerApplication.java
@@ -0,0 +1,17 @@
+package com.appchana.dos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by ivanmolera on 12/04/2018.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistrationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationServerApplication.class, args);
+    }
+}
