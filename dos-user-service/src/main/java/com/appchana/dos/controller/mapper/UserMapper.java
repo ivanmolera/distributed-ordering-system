43cf976f4fcd228c6141b57cf8292604fca74f99
Ivan Molera <molera@strands.com>
2018-04-17 10:19:36 +0200

    Adding online status to response

--- dos-user-service/src/main/java/com/appchana/dos/controller/mapper/UserMapper.java
+++ dos-user-service/src/main/java/com/appchana/dos/controller/mapper/UserMapper.java
@@ -24,6 +24,7 @@ public class UserMapper
                 .setUserId(user.getUserId())
                 .setPassword(user.getPassword())
                 .setUsername(user.getUsername())
+                .setOnlineStatus(user.getOnlineStatus())
                 .setDeleted(user.getDeleted())
                 .setContact(user.getUserContact());
 
